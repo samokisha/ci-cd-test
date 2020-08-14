a1ebd76b8e2b2713062c6ab9daafc3baa8f4dbeb
Anatoly Samokisha <anatoly@samokisha.com>
2020-08-14 13:05:27 +0300

    Added rest controller test

--- src/test/java/com/samokisha/cicdtest/CiCdTestApplicationTests.java
+++ src/test/java/com/samokisha/cicdtest/CiCdTestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.samokisha.cicdtest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CiCdTestApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void responseTest() {
+        String url = "http://localhost:" + port + "/test/request";
+        String response = restTemplate.getForObject(url, String.class);
+
+        assert response.contains(url);
     }
 
 }
