15b5355f1deb3f49714fa6436384d95182893aee
Anatoly Samokisha <anatoly@samokisha.com>
2020-08-14 16:00:11 +0300

    Added assertj

--- src/test/java/com/samokisha/cicdtest/CiCdTestApplicationTests.java
+++ src/test/java/com/samokisha/cicdtest/CiCdTestApplicationTests.java
@@ -1,5 +1,7 @@
 package com.samokisha.cicdtest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,15 +19,13 @@ class CiCdTestApplicationTests {
     private TestRestTemplate restTemplate;
 
     @Test
-    void contextLoads() {
-    }
-
-    @Test
     void responseTest() {
         String url = "http://localhost:" + port + "/test/request";
         String response = restTemplate.getForObject(url, String.class);
 
-        assert response.contains(url);
+        assertThat(response)
+            .isNotEmpty()
+            .contains(url);
     }
 
 }
